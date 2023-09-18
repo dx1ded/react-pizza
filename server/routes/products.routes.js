4ecc14a176c85832c75e65d141588629c7ef417a
dx1ded <doskovol1@gmail.com>
2023-09-17 23:24:40 -0600

    Other files

--- server/routes/products.routes.js
+++ server/routes/products.routes.js
@@ -3,18 +3,35 @@ import { AuthMiddleware } from "../auth.middleware.js"
 import { Product } from "../models/Product.js"
 
 const router = Router()
-const PRODUCTS_PER_PAGE = 8
+
+const PRODUCTS_PER_PAGE = 4
+const filterOptions = ["Meat", "Vegetarian", "Grill", "Spicy", "Closed"]
+const sortOptions = {
+  rating: { rating: -1 },
+  price_increasing: { price: 1 },
+  price_decreasing: { price: -1 }
+}
 
 router.use(json())
 
 // -> /api/products/list
-router.post("/list/:offset?", AuthMiddleware, async (req, res) => {
-  const { offset = 0 } = req.params
+router.post("/list", AuthMiddleware, async (req, res) => {
+  const { page = 1, filterBy, sortBy } = req.query
 
-  const products = await Product.aggregate([
-    { $skip: offset * PRODUCTS_PER_PAGE },
-    { $limit: PRODUCTS_PER_PAGE }
-  ])
+  const aggregator = [
+    { $skip: (page - 1) * PRODUCTS_PER_PAGE },
+    { $limit: PRODUCTS_PER_PAGE },
+  ]
+
+  if (sortBy) aggregator.unshift({
+    $sort: sortOptions[sortBy]
+  })
+
+  if (filterBy && filterBy !== "All") aggregator.unshift({
+    $match: { category: filterOptions.indexOf(filterBy) }
+  })
+
+  const products = await Product.aggregate(aggregator)
 
   res.json({ products })
 })
